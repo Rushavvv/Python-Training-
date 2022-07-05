fb932e97353a3f4480e1b551d40f8040b5ea3420
Rushav <rusu.sthapit@gmail.com>
2022-07-05 17:54:31 +0545

    :tada:Practice

--- basic/string.py
+++ basic/string.py
@@ -1,19 +1,30 @@
-print(9//3) #fps
-print(5%2) 
+# print(9//3) #fps
+# print(5%2) 
 
-#Emoji 
-#print("U+1F646")  #replace + with three zeros (000) 
-print ("\U0001F646") 
-print(round(2**3.14,3)) #round haneko to 3 dec points 
-
-
-
-print((5**3)/(4-2)*6+6) 
+# #Emoji 
+# #print("U+1F646")  #replace + with three zeros (000) 
+# print ("\U0001F646") 
+# print(round(2**3.14,3)) #round haneko to 3 sf points 
 
 
-# String Concatenation 
 
-print("Aryama" + 10)
+# print((5**3)/(4-2)*6+6) 
 
-print("Aryama" + str(10)) # turnt 10 to string 
+
+# # String Concatenation 
+
+# print("Aryama" + 10)
+
+# print("Aryama" + str(10)) # turnt 10 to string 
+
+'''
+numeric_value ==> snake_case_writing (reccomended for python) 
+numericValue ==> camelCaseWriting 
+
+'''
+
+numeric_value = 10
+print("the first value of a is: --> ",numeric_value) 
+fruit_name = "Apple" 
+print("the second value of a is: -->",fruit_name) 
 
