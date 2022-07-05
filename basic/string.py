a534db5c05187f245ee53bf410ba6a55c353d204
Rushav <rusu.sthapit@gmail.com>
2022-07-05 17:39:37 +0545

    :tada:Begin with string concatenation

---
+++ basic/string.py
@@ -0,0 +1,19 @@
+print(9//3) #fps
+print(5%2) 
+
+#Emoji 
+#print("U+1F646")  #replace + with three zeros (000) 
+print ("\U0001F646") 
+print(round(2**3.14,3)) #round haneko to 3 dec points 
+
+
+
+print((5**3)/(4-2)*6+6) 
+
+
+# String Concatenation 
+
+print("Aryama" + 10)
+
+print("Aryama" + str(10)) # turnt 10 to string 
+
