287924a800e2918ac18731e268c01848e16bdddd
Rushav <rusu.sthapit@gmail.com>
2022-07-06 17:53:39 +0545

    :hammer:Practice

--- basic/string02.py
+++ basic/string02.py
@@ -1,5 +1,24 @@
-# #Excersise 
-first_name,second_name = input("please provide your full name:").split (" ") 
-name_type = type(first_name) 
-print("Your full name is "+ first_name +" "+second_name)
-print("Type of your first name is "+ name_type)
+# # # # # #Excersise 
+# # # # first_name,second_name = input("please provide your full name:").split (" ") 
+# # # # name_type = type(first_name) 
+# # # # print("Your full name is "+ first_name +" "+second_name)
+# # # # print("Type of your first name is "+ name_type)
+# # # num_1,num_2,num_3 = input("Please input the three numbers;").split(",") 
+# # # a = int(num_1) + int(num_2) + int(num_3) 
+# # # print(a//3)
+
+# # # String Indexing 
+# name = "language"
+# # print(name[3:8]) -----> gives output guage last ko exclusive huncha
+# print (name[::-1]) #reverse didincha 
+# example = "leArN pYtHon"
+# print(example.lower())
+# print(example.upper())
+# print(example.title())
+# print(example.count("p")) #----> kati ota cha bhanha 
+
+name = input("Input your full name ")
+print(name.count("u"))
+
+
+
