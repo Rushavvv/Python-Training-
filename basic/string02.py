d9f6299300e9e1bdfdeb30c8ff941d9a0ceaf617
Rushav <rusu.sthapit@gmail.com>
2022-07-06 17:12:17 +0545

    :sparkles: two input

---
+++ basic/string02.py
@@ -0,0 +1,5 @@
+# #Excersise 
+first_name,second_name = input("please provide your full name:").split (" ") 
+name_type = type(first_name) 
+print("Your full name is "+ first_name +" "+second_name)
+print("Type of your first name is "+ name_type)
