698b923bb809872ea9c6ac6e7c48ee63acc0eb64
Rushav <rusu.sthapit@gmail.com>
2022-07-07 18:26:40 +0545

    :hammer:Contitional

---
+++ basic/conditional_statement.py
@@ -0,0 +1,67 @@
+# age = int(input("Please provide your age: -->")) 
+# if age > 18: 
+#     print(f"As your age is {age} and you can watch Thor Love & Thunder")
+# else: 
+#     print(f"As your age is {age} and you cannot watch it")
+
+
+
+
+# from random import random
+
+# import random
+# a = random.randint(0,100)
+# num = int(input("Enter a number"))
+# if a == num: 
+#     print("You win the game")
+     
+# else:
+#     if a > num: 
+#         print("The number is too low ")
+#     else:
+#         print("Number is too high ")
+# print(a)
+
+    
+# and ra or 
+
+
+'''
+
+name,age = input("Enter your name and age").split(",")
+actual_name = str(name.lower())
+actual_age = int(age)
+if actual_name[0] == "a" and actual_age > 18:  
+    print("You can watch the movie")
+else: 
+    print("Print you cannot watch the movie") 
+    
+
+'''
+
+# age = int(input("enter age"))
+# if 0<age<5: 
+#     print("The ticket is free")
+# elif 5<age<60: 
+#     print("The cost is 200")
+# elif age>60:
+#     print("The ticket is 100")
+# else: 
+#     print("You are estupid")
+
+'''
+check for empty string 
+
+'''
+
+fname = input("Enter your name ")
+sname = "" 
+
+if fname:
+    if "a" in fname: 
+     print("'h' is present in your name")
+    else:
+        print("Not present") 
+else: 
+     print("Please input your name") 
+
